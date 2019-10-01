b0cc139ac5f95edbb42b9f6b1432f89e4d5d23b0
Captain-K <44168298+Captain-K-101@users.noreply.github.com>
2019-10-01 18:12:56 +0530

    Update Driver.java
    added try catch blocks

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 
 public class Driver {
-
+try{
 	public static void main(String[] args) {
 		Point p = new Point();
 	      p.setX(2);
@@ -17,8 +17,14 @@ public class Driver {
 			else
 				System.out.println("Different"); // This line is always executed
 		  
-		  System.out.println(p);	
-		
+		  System.out.println(p);
+	}catch(IOException e){
+	System.out.println(e);
+	}catch(SQLException e){
+	System.out.println(e);
+	}catch(Error e){
+	System.out.println(e);
+	}
 	}
 
 }
